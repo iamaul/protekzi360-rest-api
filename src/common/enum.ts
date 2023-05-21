48e913a0027c22ed298db51c73ff5b81f9c8b66f
Agung Maulana <agung.maulana@tech.jago.com>
2023-05-22 05:15:21 +0700

    chore: add PaymentStatus & PaymentType enum

--- src/common/enum.ts
+++ src/common/enum.ts
@@ -3,3 +3,17 @@ export enum Roles {
   USER = 'user',
   DEVELOPER = 'developer',
 }
+
+export enum PaymentStatus {
+  PENDING = 'pending',
+  COMPLETED = 'completed',
+  EXPIRED = 'expired',
+  FAILED = 'failed',
+  CANCELED = 'canceled',
+}
+
+export enum PaymentType {
+  BANK_TRANSFER = 'bank_transfer',
+  E_CHANNEL = 'echannel',
+  PERMATA = 'permata',
+}
