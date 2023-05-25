cfd64547f18e9231e6afec8616a1e3028bd2e30d
Agung Maulana <agung.maulana@tech.jago.com>
2023-05-25 14:00:15 +0700

    chore: remove MidtransService from payment module

--- src/domain/payment/payment.module.ts
+++ src/domain/payment/payment.module.ts
@@ -4,11 +4,10 @@ import { PaymentService } from './payment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentMethodEntity, UserPaymentEntity } from '../../typeorm';
 import { ConfigFirebase } from '../../config/config.firebase';
-import { MidtransService } from '../../midtrans/midtrans.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PaymentMethodEntity, UserPaymentEntity])],
-  providers: [PaymentService, MidtransService, ConfigFirebase],
+  providers: [PaymentService, ConfigFirebase],
   controllers: [PaymentController],
 })
 export class PaymentModule {}
