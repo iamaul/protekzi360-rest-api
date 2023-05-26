5ebf26c43cf35c1ecd76c1f1320e137345ef57f1
Agung Maulana <agung.maulana@tech.jago.com>
2023-05-26 16:50:10 +0700

    chore: add additional typeorm module

--- src/domain/payment/payment.module.ts
+++ src/domain/payment/payment.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PaymentMethodEntity, UserPaymentEntity } from '../../typeorm';
+import {
+  PaymentMethodEntity,
+  UserEntity,
+  UserMetadataEntity,
+  UserPaymentEntity,
+} from '../../typeorm';
 import { ConfigFirebase } from '../../config/config.firebase';
 import { UserService } from '../user/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaymentMethodEntity, UserPaymentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      PaymentMethodEntity,
+      UserPaymentEntity,
+      UserMetadataEntity,
+      UserEntity,
+    ]),
+  ],
   providers: [PaymentService, UserService, ConfigFirebase],
   controllers: [PaymentController],
 })
