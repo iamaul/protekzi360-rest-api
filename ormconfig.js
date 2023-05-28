e84ea3108fe467a43cfe6bce5fff424d05b9e746
Agung Maulana <agung.maulana@tech.jago.com>
2023-05-28 19:35:02 +0700

    chore: update app.yml gcp

--- ormconfig.js
+++ ormconfig.js
@@ -2,15 +2,20 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_TYPE, DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME } = process.env;
+const {
+  DB_TYPE,
+  INSTANCE_UNIX_SOCKET,
+  MY_DB_USER,
+  MY_DB_PASSWORD,
+  MY_DATABASE,
+} = process.env;
 
 module.exports = {
   type: DB_TYPE,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
+  host: INSTANCE_UNIX_SOCKET,
+  username: MY_DB_USER,
+  password: MY_DB_PASSWORD,
+  database: MY_DATABASE,
   migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
   entities: [__dirname + '/src/**/*.entity.{ts,js}'],
 };
